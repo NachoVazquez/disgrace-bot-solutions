1b29b1897ac1893a3b387c12c87bb13b833c9bf1
Nacho Vazquez <nacho.vazquez.dev@gmail.com>
2020-05-21 22:18:37 -0300

    disconect bot form voice channel when everyone leaves

--- apps/disgrace-bot/src/app/events/entered-disgrace.ts
+++ apps/disgrace-bot/src/app/events/entered-disgrace.ts
@@ -1,5 +1,4 @@
-import { Client, StreamDispatcher } from 'discord.js';
-import * as fs from 'fs';
+import { Client } from 'discord.js';
 import * as path from 'path';
 
 import * as disgracers from '../../assets/json/disgracers.json';
@@ -9,48 +8,38 @@ export function enteredDisgrace(client: Client): void {
     const newUserChannel = newMember.channel;
     const oldUserChannel = oldMember.channel;
 
-    let dispatcher: StreamDispatcher;
-
     if (!oldUserChannel && !!newUserChannel) {
       const connection = await newUserChannel.join();
 
       switch (newMember.member.id) {
         case disgracers['Marcel']:
-          dispatcher = connection.play(
-            path.join(__dirname, 'assets/sounds/marcel.mp3')
-          );
+          connection.play(path.join(__dirname, 'assets/sounds/marcel.mp3'));
           break;
         case disgracers['Norby']:
-          dispatcher = connection.play(
-            path.join(__dirname, 'assets/sounds/norby.mp3')
-          );
+          connection.play(path.join(__dirname, 'assets/sounds/norby.mp3'));
           break;
         case disgracers['Mauro']:
-          dispatcher = connection.play(
-            path.join(__dirname, 'assets/sounds/mauro.mp3')
-          );
+          connection.play(path.join(__dirname, 'assets/sounds/mauro.mp3'));
           break;
         case disgracers['Nacho']:
-          dispatcher = connection.play(
-            path.join(__dirname, 'assets/sounds/yo.mp3')
-          );
+          connection.play(path.join(__dirname, 'assets/sounds/yo.mp3'));
           break;
         case disgracers['Pedro']:
-          dispatcher = connection.play(
-            path.join(__dirname, 'assets/sounds/pedro.mp3')
-          );
+          connection.play(path.join(__dirname, 'assets/sounds/pedro.mp3'));
           break;
         case disgracers['Aldo']:
-          dispatcher = connection.play(
-            path.join(__dirname, 'assets/sounds/aldo.mp3')
-          );
+          connection.play(path.join(__dirname, 'assets/sounds/aldo.mp3'));
           break;
 
         default:
           break;
       }
-    } else if (newUserChannel === undefined) {
-      // User leaves a voice channel
+    } else if (
+      !newUserChannel &&
+      oldUserChannel.members.size === 1 &&
+      oldUserChannel.members.firstKey() === client.user.id
+    ) {
+      oldUserChannel.leave();
     }
   });
 }
